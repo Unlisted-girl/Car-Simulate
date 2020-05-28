c63ba6fc3d511f54eb873ff7accadaf1e1bf31aa
Unlisted-girl <65407899+Unlisted-girl@users.noreply.github.com>
2020-05-28 10:52:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Car-Simulate
-Simple car simulator to calculate deformation 
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,38 @@
+var car, wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(55,90);
+  weight = random(500,1500);
+  car = createSprite(50, 200, 50, 70);
+  wall = createSprite(1500, 200, 60, height/2);
+}
+
+function draw() {
+  background(255,255,255);  
+
+  car.velocityX = speed;
+
+  if(wall.x - car.x < (wall.width + car.width)/2){
+     
+    car.velocityX = 0;
+    var deformation = 0.5* weight* speed * speed/ 22500
+
+    if(deformation > 180){
+      car.shapeColor = "red";
+    }
+
+    if(deformation> 100 && deformation < 180){
+      car.shapeColor = "yellow";
+    }
+
+    if (deformation < 100){
+      car.shapeColor = "green";
+    }
+  }
+
+
+  drawSprites();
+}
